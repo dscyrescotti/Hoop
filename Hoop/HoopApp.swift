8c4de5df60962885908d2d731a391f149e5da5db
dscyrescotti <dscyrescotti@gmail.com>
2023-01-21 13:35:14 +0630

    feat: commit initially

---
+++ Hoop/HoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoopApp.swift
+//  Hoop
+//
+//  Created by Aye Chan on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct HoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
